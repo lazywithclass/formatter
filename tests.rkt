ce8d119b674cf78f069f6a6a81b8456be175f0ba
Alberto Zaccagni <montecristoh@gmail.com>
2017-12-20 19:10:22 +0100

    oops committed commented tests

--- tests.rkt
+++ tests.rkt
@@ -9,34 +9,34 @@
 ;; we call a the formatted string
 ;; b the string to be formatted
 
-;; (test-case
-;;     "b does not contain spaces"
-;;   (let ([a '(#\1 #\space #\space #\2 #\newline)]
-;;         [b '(#\4 #\5 #\newline)])
-;;     (check-equal? (format a b #f) '(#\4 #\space #\space #\5 #\newline #\newline))))
+(test-case
+    "b does not contain spaces"
+  (let ([a '(#\1 #\space #\space #\2 #\newline)]
+        [b '(#\4 #\5 #\newline)])
+    (check-equal? (format a b #f) '(#\4 #\space #\space #\5 #\newline #\newline))))
 
-;; (test-case
-;;     "a is on multiple lines"
-;;   ;;;; a:
-;;   ;; 1
-;;   ;;  2
-;;   ;;   3
-;;   (let ([a '(#\1 #\newline #\space #\2 #\newline #\space #\space #\3 #\newline)]
-;;         [b '(#\4 #\5 #\6 #\newline)])
-;;     (check-equal? (format a b #f) '(#\4 #\newline #\space #\5 #\newline #\space #\space #\6 #\newline #\newline))))
+(test-case
+    "a is on multiple lines"
+  ;;;; a:
+  ;; 1
+  ;;  2
+  ;;   3
+  (let ([a '(#\1 #\newline #\space #\2 #\newline #\space #\space #\3 #\newline)]
+        [b '(#\4 #\5 #\6 #\newline)])
+    (check-equal? (format a b #f) '(#\4 #\newline #\space #\5 #\newline #\space #\space #\6 #\newline #\newline))))
 
-;; (test-case
-;;     "b is formatted in a wrong fashion"
-;;   ;;;; a:
-;;   ;; 1
-;;   ;;  2
-;;   ;;   3
-;;   ;;;; b:
-;;   ;; 4 5
-;;   ;;  6
-;;   (let ([a '(#\1 #\newline #\space #\2 #\newline #\space #\space #\3 #\newline)]
-;;         [b '(#\4 #\space #\5 #\newline #\space #\6 #\newline)])
-;;     (check-equal? (format a b #f) '(#\4 #\newline #\space #\5 #\newline #\space #\space #\6 #\newline #\newline))))
+(test-case
+    "b is formatted in a wrong fashion"
+  ;;;; a:
+  ;; 1
+  ;;  2
+  ;;   3
+  ;;;; b:
+  ;; 4 5
+  ;;  6
+  (let ([a '(#\1 #\newline #\space #\2 #\newline #\space #\space #\3 #\newline)]
+        [b '(#\4 #\space #\5 #\newline #\space #\6 #\newline)])
+    (check-equal? (format a b #f) '(#\4 #\newline #\space #\5 #\newline #\space #\space #\6 #\newline #\newline))))
 
 (test-case
     "a contains a comment"
